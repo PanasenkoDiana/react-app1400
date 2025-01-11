2a0f6b2c5b64692451c96f7e87d7dd08cb73ef75
arman455 <snositel99@gmail.com>
2025-01-11 22:31:21 +0200

    added comments about searchbar

--- src/shared/SearchBar/SearchBar.tsx
+++ src/shared/SearchBar/SearchBar.tsx
@@ -3,39 +3,39 @@ import "./SearchBar.css";
 import { useProducts } from "../../hooks/useProducts";
 import { SearchProduct } from "../SearchProduct/SearchProduct";
 
-interface ISearchBarProps {
+interface ISearchBarProps { // створення інтерфейсу для отримання елементів пошуку з header
     setSearch: (value: string) => void;
     search: string;
   }
 
 export function SearchBar({setSearch, search}: ISearchBarProps){
 
-    const [isOpen, setIsOpen] = useState(false);
-    const modalRef = useRef<HTMLDivElement | null>(null);
+    const [isOpen, setIsOpen] = useState(false); // створення стану для відкриття модального вікна
+    const modalRef = useRef<HTMLDivElement | null>(null); // створення посилання на елемент
 
-    const {products} = useProducts()
+    const {products} = useProducts() // виклик хука для отримання продуктів
   
-    const handleFocus = () => {
+    const handleFocus = () => { // функція для відкриття модального вікна
       setIsOpen(true);
     };
 
-    const handleExit = () => {
+    const handleExit = () => { // функція для закриття модального вікна
         setIsOpen(false);
       };
   
-    useEffect(() => {
-      const handleClickOutside = (event: MouseEvent) => {
+    useEffect(() => { 
+      const handleClickOutside = (event: MouseEvent) => { // функція для закриття модального вікна при кліку за межами вікна
         if (
-          modalRef.current &&
-          !modalRef.current.contains(event.target as Node)
+          modalRef.current && // якщо наш елемент існує і заперечення того, що наш елемент є дочірнім елементом на який ми натиснули
+          !modalRef.current.contains(event.target as Node) 
         ) {
-          setIsOpen(false);
+          setIsOpen(false); // закриття модального вікна
         }
       };
   
-      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener("mousedown", handleClickOutside); // додавання слухача подій для закриття модального вікна
       return () => {
-        document.removeEventListener("mousedown", handleClickOutside);
+        document.removeEventListener("mousedown", handleClickOutside); // видалення слухача подій
       };
     }, []);
   
@@ -46,13 +46,13 @@ export function SearchBar({setSearch, search}: ISearchBarProps){
           placeholder="Пошук продуктів..."
           onFocus={handleFocus}
           className="input"
-          onChange={(event)=>{setSearch(event.target.value)}}
+          onChange={(event)=>{setSearch(event.target.value)}} // присвоєння функції для пошуку продуктів, які відповідають введеному значенню
         />
   
         {isOpen && (
-          <div ref={modalRef} className="search-modal">
+          <div ref={modalRef} className="search-modal">  {/* ref={modalRef} - отримуємо значення елементу */}
             <ul>
-                {products.map((product) => {
+                {products.map((product) => { // перебір продуктів, відображення тих, які відповідають введеному значенню
                     return (search === '' || product.title.toLowerCase().includes(search.toLowerCase())) ? (
                         <li className="search-field" key={product.id} onClick={handleExit}>
                             <SearchProduct
